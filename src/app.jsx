87695110505cd5459b027c63dd646e536b2b6028
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-03-31 14:31:04 -0300

    ✔  Estilos Inicias / Marcação Inicial

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,22 @@
 const App = () => {
- return <h1>Vite + React Boilerplate</h1>
+  return (
+    <div className="app">
+      <main className="main">
+        <div>
+          <h4>Pergunta</h4>
+          <ul className="options">
+            <li><button className="btn btn-option">Alternativa A</button></li>
+            <li><button className="btn btn-option">Alternativa B</button></li>
+            <li><button className="btn btn-option">Alternativa C</button></li>
+            <li><button className="btn btn-option">Alternativa D</button></li>
+          </ul>
+        </div>
+        <div>
+          <button className="btn btn-ui">Próxima</button>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export { App }
