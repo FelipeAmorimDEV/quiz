3f1e67e8a6150065c534f3f98466e4ec8b4f728e
FelipeAmorimDEV <felipecastroo.ads@gmail.com>
2024-03-31 14:42:07 -0300

    ✔  Fetch das perguntas

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,30 @@
+import { useReducer } from "react"
+import { useEffect } from "react"
+
+const reducer = (state, action) => {
+  if (action.type === 'fetch_questions') {
+    return {...state, apiData: action.payload}
+  }
+
+  return state
+} 
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, { apiData: [] })
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/FelipeAmorimDEV/fake-data/main/videogame-questions.json')
+      .then(r => r.json())
+      .then(data => dispatch({ type: 'fetch_questions', payload: data }))
+      .catch(error => alert(error.message))
+  }, [])
+
   return (
     <div className="app">
       <main className="main">
-        <div>
+        {state.apiData.length > 0 && 
+          <>
+          <div>
           <h4>Pergunta</h4>
           <ul className="options">
             <li><button className="btn btn-option">Alternativa A</button></li>
@@ -14,6 +36,8 @@ const App = () => {
         <div>
           <button className="btn btn-ui">Próxima</button>
         </div>
+          </>
+        }
       </main>
     </div>
   )
